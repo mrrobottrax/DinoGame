120c3f6fb26eb7ac661b399087b44590dfb1c025
Andrew <mrrobottrax@gmail.com>
2025-09-30 03:28:01 -0400

    Fix shader compilation warnings

--- DinoGUI/shaders/RootSignature.hlsli
+++ DinoGUI/shaders/RootSignature.hlsli
@@ -5,7 +5,7 @@
 #define TextureRootSignature \
     "RootFlags(0), " \
     "RootConstants(num32BitConstants=20, b0), " \
-    "SRV(t0), " \
+    "DescriptorTable(SRV(t0, flags=DATA_STATIC)), " \
     "StaticSampler(s0)"
 
 cbuffer RectConstantBuffer : register(b0)
--- DinoGUI/shaders/TexturePixel.hlsl
+++ DinoGUI/shaders/TexturePixel.hlsl
@@ -1,8 +1,12 @@
 #include "RootSignature.hlsli"
 #include "Texture.hlsli"
 
+Texture2D mainTex : register(t0);
+SamplerState mainSampler : register(s0);
+
 [RootSignature(TextureRootSignature)]
 float4 main(VertexOut vin) : SV_TARGET
 {
+	//return mainTex.Sample(mainSampler, vin.uv);
 	return float4(vin.uv, 0, 1);
 }
