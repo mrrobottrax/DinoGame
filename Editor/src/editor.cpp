1aabd4dbc20b193d0cbbbaf06fa9c607019fcbba
Andrew <mrrobottrax@gmail.com>
2025-10-22 18:18:12 -0400

    Create editor project

---
+++ Editor/src/editor.cpp
@@ -0,0 +1,39 @@
+#include "pch.h"
+
+GAME_API GameInfo get_game_info() {
+  return {
+      .WindowName = "Test Game",
+  };
+}
+
+GAME_API void load_main_menu() {
+  console_log("Loading main menu");
+
+  /*
+  g_ILevelSystem->set_skybox("Menu_Sky.png");
+
+  UI_ColoredPanel &mainMenuCentred = ui_create<UI_ColoredPanel>();
+  mainMenuCentred->Flags |= UI_PANEL_FLAG_SUBTRACTIVE_SIZE_Y;
+  mainMenuCentred->set_anchor(0.5f, 0);
+  mainMenuCentred->set_pivot(0.5f, 0);
+  mainMenuCentred->set_position(0, 0);
+  mainMenuCentred->set_dimensions(800, 0);
+  mainMenuCentred->set_color(1, 1, 1, 0.1f);
+
+  UI_Image &image = ui_create<UI_Image>(mainMenuCentred);
+  image->set_texture("EngineLogo.png");
+  image->Anchor[0] = 0.5f;
+  image->Anchor[1] = 1;
+  image->set_position_dimensions(0, -80, 900, 400, 0.5f, 1);
+
+  DyanmicProp &cube = entity_spawn<DyanmicProp>();
+  cube->set_position(0, 0, 0);
+  cube->set_model("MenuCube.gltf");
+  cube->set_animation("spin");
+
+  Camera &camera = entity_spawn<Camera>();
+  camera.set_position(0, -5, 0);
+
+  playsound_loop("menu.wav", SND_MUSIC);
+  */
+}
